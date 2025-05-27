e70ffffd14a8b07079162f41a7f7040aaabc54b2
Sumit Kumar <sumitkumarbittuair@gmail.com>
2025-05-27 22:35:47 +0530

    html

--- server.cpp
+++ server.cpp
@@ -1,13 +1,23 @@
 #include "cpp-httplib/httplib.h"
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string read_file(const std::string& filename) {
+    std::ifstream file(filename);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 int main() {
     using namespace httplib;
     Server svr;
 
     svr.Get("/", [](const Request&, Response& res) {
-        res.set_content("Hello from C++ backend!", "text/plain");
+        std::string html = read_file("index.html");
+        res.set_content(html, "text/html");
     });
 
     int port = std::getenv("PORT") ? std::stoi(std::getenv("PORT")) : 8080;
